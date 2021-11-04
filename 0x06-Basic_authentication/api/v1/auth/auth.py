ce21a7507fe8f464ff754381d831771c2ef07a4b
vic-1998 <victor.mplaza98@gmail.com>
2021-11-04 10:20:59 -0500

    Basic authentication

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """Manages API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns true if a route requires authorization,
+        false otherwise.
+        """
+        if not path or not excluded_paths or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path = path + '/'
+        if path in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Checks if request contains authorization in its
+        header.
+        """
+        if not request:
+            return None
+        if not request.headers.get('Authorization'):
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user method
+        """
+        return None
